c45d02e8197468efe7adf45d24329467d75e3067
sshahriar143 <shimulshahriar47@gmail.com>
2023-03-27 02:34:54 +0600

    loadData from api

--- reactjs-core-concepts2/src/App.js
+++ reactjs-core-concepts2/src/App.js
@@ -1,15 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
+     {/* <Counter></Counter> */}
     </div>
   );
+}
+
+function ExternalUsers (){
+  const [users, setUsers] =useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data=>setUsers(data))
+  },[])
+  return(
+    <div>
+      <h2>External Users</h2>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User> )
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div className='user'>
+      <h1>Name:{props.name}</h1>
+      <h2>Email:{props.email}</h2>
+    </div>
+  )
 }
 
 function Counter(){
